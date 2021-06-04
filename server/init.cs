55ff3ef98a586f39494809eb0accf7a6b2460d7b
Electrk <Electrk@ymail.com>
2021-06-03 21:06:11 -0700

    Cleaned up server/init.cs

--- server/init.cs
+++ server/init.cs
@@ -1,54 +1,75 @@
+// -----------------------------------------------------------------------------
+//  Torque Game Engine
+//  Copyright (C) GarageGames.com, Inc.
+// -----------------------------------------------------------------------------
+
+
 function initServer ()
 {
 	%dashes = "";
 	%version = atoi ($Version);
 	%version = mClampF (%version, 0, 25);
-	%i = 0;
-	while (%i < %version)
+
+	for ( %i = 0; %i < %version; %i++ )
 	{
 		%dashes = %dashes @ "-";
-		%i += 1;
 	}
+
 	echo ("\n--------- Initializing Base: Server " @ %dashes);
-	$useSteam = 0;
+
+	// Initialize the Steam API, if available.
+	$useSteam = false;
 	$useSteam = SteamAPI_Init ();
-	if ($useSteam)
+
+	if ( $useSteam )
 	{
 		echo ("Steam initialized...");
 	}
+
+	// Neither of these appear to have any function in Blockland.
 	$Server::Status = "Unknown";
-	$Server::TestCheats = 1;
+	$Server::TestCheats = true;
+
+	// The common module provides the basic server functionality.
 	initBaseServer ();
+
+	// Load up game server support script.
 	exec ("./scripts/game.cs");
 }
 
 function initDedicated ()
 {
-	enableWinConsole (1);
+	enableWinConsole (true);
+
 	echo ("\n--------- Starting Dedicated Server ---------");
-	$Server::Dedicated = 1;
-	$Server::LAN = 0;
+
+	$Server::Dedicated = true;
+	$Server::LAN = false;
+
 	createServer ("Internet");
 }
 
 function initDedicatedLAN ()
 {
-	enableWinConsole (1);
+	enableWinConsole (true);
+
 	echo ("\n--------- Starting Dedicated LAN Server ---------");
-	$Server::Dedicated = 1;
-	$Server::LAN = 1;
+
+	$Server::Dedicated = true;
+	$Server::LAN = true;
+
 	createServer ("LAN");
 }
 
 function serverPart2 ()
 {
-	if ($Server::Dedicated)
+	if ( $Server::Dedicated )
 	{
-		if ($Server::LAN)
+		if ( $Server::LAN )
 		{
 			initDedicatedLAN ();
 		}
-		else if ($Server::LAN)
+		else if ( $Server::LAN )
 		{
 			initDedicatedLAN ();
 		}
@@ -61,17 +82,20 @@ function serverPart2 ()
 	{
 		initClient ();
 	}
-	if ($connectArg !$= "" && !$Server::Dedicated)
+
+	if ( $connectArg !$= "" && !$Server::Dedicated )
 	{
 		Connecting_Text.setText ("Connecting to " @ $connectArg);
 		Canvas.pushDialog (connectingGui);
 	}
-	if ($GameModeArg !$= "" && !$Server::Dedicated)
+
+	if ( $GameModeArg !$= "" && !$Server::Dedicated )
 	{
 		setTimeScale (10);
+
 		$Pref::Net::ServerType = "SinglePlayer";
+
 		createServer ($Pref::Net::ServerType);
-		ConnectToServer ("local", $Pref::Server::Password, 1, 0);
+		ConnectToServer ("local", $Pref::Server::Password, true, false);
 	}
 }
-
